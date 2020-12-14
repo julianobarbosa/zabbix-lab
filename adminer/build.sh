e59f9e4af42bf6d5630d5f627edbc433fb1c488d
Juliano Barbosa <julianomb@gmail.com>
2020-12-14 16:21:03 -0300

    added adminer

---
+++ adminer/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -eux
+
+VERSION=4.7.6
+MINOR_VERSION=4.7
+MAJOR_VERSION=4
+
+IMAGE_NAME=dehy/adminer:latest hooks/build
+docker tag dehy/adminer:latest dehy/adminer:$MAJOR_VERSION
+docker tag dehy/adminer:latest dehy/adminer:$MINOR_VERSION
+docker tag dehy/adminer:latest dehy/adminer:$VERSION
+
+docker push dehy/adminer:$MAJOR_VERSION
+docker push dehy/adminer:$MINOR_VERSION
+docker push dehy/adminer:$VERSION
+docker push dehy/adminer:latest
